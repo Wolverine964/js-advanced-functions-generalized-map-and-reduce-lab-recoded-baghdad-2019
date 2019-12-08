2abf5beb5716b5576cda3cc882dd756c6a21ea27
ibraheem Hasan <rules656@gmail.com>
2019-12-08 19:16:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map (arr, action){
+    let sol=[]
+    for (let i=0; i < arr.length; i++){
+        sol.push(action(arr[i]))
+    }
+    return sol
+}
+
+function reduce(arr, action, stratingPiont=0){
+    let sol= (!!stratingPiont) ? stratingPiont : arr[0]
+    let i= (!!stratingPiont) ? 0 : 1
+    for (; i < arr.length; i++){
+        sol = action(sol, arr[i])
+    }
+    return sol
+}
