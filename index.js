bf027a19a45b7aa25c06862699f75b59978da7bc
ibraheem Hasan <rules656@gmail.com>
2019-12-08 19:18:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ function map (arr1, action){
     for (let i=0; i < arr1.length; i++){
         ans.push(action(arr1[i]))
     }
-    return ans
-}
-
+    return ans}
 function reduce(arr1, action, sp=0){
     let ans= (!!sp) ? sp : arr1[0]
     let i= (!!sp) ? 0 : 1
     for (; i < arr1.length; i++){
-        ans = action(sol, arr1[i])
+        ans = action(ans, arr1[i])
     }
-    return sol
+    return ans
 }
